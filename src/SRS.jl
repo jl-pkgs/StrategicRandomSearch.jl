c13123468daf481c6cd4dc22afa68f34dc002404
Dongdong Kong <kongdd@users.noreply.github.com>
2026-03-03 10:13:26 +0800

    improve eps

--- src/SRS.jl
+++ src/SRS.jl
@@ -10,8 +10,8 @@ function SRS(
     po::Int=guess_po(p),      # Fig 2a, po optimal points (yellow squares), 精英中的精英
     # deps::Int=12,           # x 空间收缩终止条件
     delta::Float64=0.01,      # 区间收缩因子, `delta_Mbounds = Mbounds * delta`
-    f_atol::Float64=1e-5,
-    eps::Int=4,               # 进入/维持某些全局跳出逻辑
+    f_atol::Float64=1e-5,       # 终止条件：全局最优值收敛
+    f_atol_inner::Float64=1e-4, # 低于该阈值则进入精细搜索
     update_eps::Bool=true,
     λ_short::Float64=0.02, λ_long::Float64=0.2, # 洗牌强度 决定收敛速度
     init_loop_min::Int=3, loop_min::Int=2,
@@ -45,7 +45,8 @@ function SRS(
     num_call = calculate_goal!(y_cand, fn, X_cand) # update y_cand
     y_opt, X_opt, X_worst = select_optimal(y_cand, X_cand; p) # Optimal: 精英点
 
-    neps = eps
+    n_eps = ceil(Int, -log10(f_atol_inner)) # f_atol: 1e-5 -> 5
+    n_eps_back = n_eps
 
     _fevals_loops = Float64[] # 每次loop的最优值
     feval_iters = Float64[]
@@ -72,7 +73,7 @@ function SRS(
         loop += 1
         current_loop_min = inner_search_started ? loop_min : init_loop_min
 
-        λ = (eps > neps + 2) ? λ_short : λ_long
+        λ = (n_eps > n_eps_back + 2) ? λ_short : λ_long
         shuffle_cand!(X_cand_out, X_opt, X_worst, Mbounds, lb, ub; p, λ) # 洗牌
 
         num_call = calculate_goal!(y_cand_out, fn, X_cand_out, num_call)
@@ -97,10 +98,16 @@ function SRS(
         if loop > current_loop_min
             f_opt = _fevals_loops[loop-current_loop_min] # 不能是邻近的
             # isapprox(f_opt, feval; atol=f_atol) && break # _f_best收敛
-            ineed = abs(nanminimum(feval_iters[num_iter-current_loop_min+1]) - nanminimum(feval_iters[num_iter]))
+            f_prev = nanminimum(feval_iters[num_iter-current_loop_min+1])
+            f_curr = nanminimum(feval_iters[num_iter])
+            ineed = abs(f_prev - f_curr)
+            ## 同时增加一个结束判断
         end
 
-        if (loop > current_loop_min) && abs(log10(nanmax(ineed, 10.0^(-eps - 1)))) ≥ eps
+        eps = 10.0^(-n_eps)
+        eps_floor = eps / 10.0
+
+        if (loop > current_loop_min) && nanmax(ineed, eps_floor) <= eps
             # abs(nanminimum(log10.(Mbounds ./ M))) > deps # M收缩过多
             inner_search_started = true
 
@@ -149,9 +156,7 @@ function SRS(
             # 率定水文模型不开这个部分（因为水文模型要求精度不高，打开会使前面的等距搜索太慢了）
             # 检查是否需要更新 eps
             xneed = abs(y_opt[1] - feval_iters[num_iter])
-            if (abs(log10(nanmax(xneed, 10.0^(-eps - 1)))) ≥ eps && update_eps)
-                eps += 1
-            end
+            (nanmax(xneed, eps_floor) <= eps && update_eps) && (n_eps += 1)
         end
         push!(_fevals_loops, _feval) # _fevals_loop[loop] = feval
     end
